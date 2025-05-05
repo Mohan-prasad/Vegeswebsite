b9dc87689babf14a1712213ff6afb5ab0ff5441d
Mohan Prasad <medamanoori99@gmail.com>
2025-05-05 08:25:05 +0800

    add static root for heroku deployment

---
+++ Procfile
@@ -0,0 +1,1 @@
+web : gunicorn veges.wsgi
--- veges/settings.py
+++ veges/settings.py
@@ -1,5 +1,8 @@
 
 import os
+import django_heroku
+import dj_database_url
+from decouple import config
 
 from pathlib import Path
 
@@ -39,6 +42,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'veges.urls'
@@ -113,7 +117,15 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
   ]
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+django_heroku.settings(locals())
+
+
